d027cc4b59b9708bedfc5d127ce539acc2d65380
LuisLVar <3005139410101@ingenieria.usac.edu.gt>
2018-12-03 20:21:22 -0600

    Conexión y Consulta SQL

---
+++ ProyectoArquidiocesis/ProyectoArquidiocesis/Datos/ConsultaSQL.cs
@@ -0,0 +1,163 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoArquidiocesis.Datos
+{
+    class ConsultaSQL
+    {
+        public void NuevoMatrimonio(string id, string fecha, string notario, string novio, string novia, string urlDoc, string hashcode)
+        {
+            //crear un objeto conexion
+            MySqlConnection conectar = Conexion.getConexion();
+
+            try
+            {
+                //Abrir conexion
+                conectar.Open();
+
+                //crear objeto de tipo MySqlCommand
+                MySqlCommand comando = new MySqlCommand();
+
+                //Enviar la cadena conexión
+                comando.Connection = conectar;
+
+
+                //Pondremos tipo text y enviar sql
+                comando.CommandType = System.Data.CommandType.Text;
+
+                comando.CommandText = "INSERT INTO MATRIMONIO VALUES(@a, @b, @c, @d, @e, @f, @g);";
+
+                //Agregar los parametros
+                comando.Parameters.AddWithValue("@a", id);
+                comando.Parameters.AddWithValue("@b", fecha);
+                comando.Parameters.AddWithValue("@c", notario);
+                comando.Parameters.AddWithValue("@d", novio);
+                comando.Parameters.AddWithValue("@e", novia);
+                comando.Parameters.AddWithValue("@f", urlDoc);
+                comando.Parameters.AddWithValue("@g", hashcode);
+
+                //Ejecutar comando sql
+                try
+                {
+                    comando.ExecuteNonQuery();
+                    conectar.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    Console.WriteLine("Entro catch1");
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("Entro catch2");
+            }
+        }
+
+        public void NuevoBautismo(string id, string fecha, string notario, string bautizado, string urlDoc, string hashcode)
+        {
+            //crear un objeto conexion
+            MySqlConnection conectar = Conexion.getConexion();
+
+            try
+            {
+                //Abrir conexion
+                conectar.Open();
+
+                //crear objeto de tipo MySqlCommand
+                MySqlCommand comando = new MySqlCommand();
+
+                //Enviar la cadena conexión
+                comando.Connection = conectar;
+
+
+                //Pondremos tipo text y enviar sql
+                comando.CommandType = System.Data.CommandType.Text;
+
+                comando.CommandText = "INSERT INTO CONFIRMACION VALUES(@a, @b, @c, @d, @e, @f);";
+
+                //Agregar los parametros
+                comando.Parameters.AddWithValue("@a", id);
+                comando.Parameters.AddWithValue("@b", fecha);
+                comando.Parameters.AddWithValue("@c", notario);
+                comando.Parameters.AddWithValue("@d", bautizado);
+                comando.Parameters.AddWithValue("@e", urlDoc);
+                comando.Parameters.AddWithValue("@f", hashcode);
+
+                //Ejecutar comando sql
+                try
+                {
+                    comando.ExecuteNonQuery();
+                    conectar.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    Console.WriteLine("Entro catch1");
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("Entro catch2");
+            }
+        }
+
+        public void NuevaConfirmacion(string id, string fecha, string notario, string confirmado, string urlDoc, string hashcode)
+        {
+            //crear un objeto conexion
+            MySqlConnection conectar = Conexion.getConexion();
+
+            try
+            {
+                //Abrir conexion
+                conectar.Open();
+
+                //crear objeto de tipo MySqlCommand
+                MySqlCommand comando = new MySqlCommand();
+
+                //Enviar la cadena conexión
+                comando.Connection = conectar;
+
+
+                //Pondremos tipo text y enviar sql
+                comando.CommandType = System.Data.CommandType.Text;
+
+                comando.CommandText = "INSERT INTO CONFIRMACION VALUES(@a, @b, @c, @d, @e, @f);";
+
+                //Agregar los parametros
+                comando.Parameters.AddWithValue("@a", id);
+                comando.Parameters.AddWithValue("@b", fecha);
+                comando.Parameters.AddWithValue("@c", notario);
+                comando.Parameters.AddWithValue("@d", confirmado);
+                comando.Parameters.AddWithValue("@e", urlDoc);
+                comando.Parameters.AddWithValue("@f", hashcode);
+
+                //Ejecutar comando sql
+                try
+                {
+                    comando.ExecuteNonQuery();
+                    conectar.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    Console.WriteLine("Entro catch1");
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("Entro catch2");
+            }
+        }
+    }
+}
--- ProyectoArquidiocesis/ProyectoArquidiocesis/Form1.Designer.cs
+++ ProyectoArquidiocesis/ProyectoArquidiocesis/Form1.Designer.cs
@@ -28,14 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(155, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +52,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
