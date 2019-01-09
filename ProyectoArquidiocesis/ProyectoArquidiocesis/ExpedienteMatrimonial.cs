2c74366c5256b2aa77bc0938bb69eb9443ab47e3
Jose Herrera <31715468+joseherrera7@users.noreply.github.com>
2019-01-09 08:26:01 -0600

    Update ExpedienteMatrimonial.cs

--- ProyectoArquidiocesis/ProyectoArquidiocesis/ExpedienteMatrimonial.cs
+++ ProyectoArquidiocesis/ProyectoArquidiocesis/ExpedienteMatrimonial.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Microsoft.Office.Interop.Word;
+using MySql.Data.MySqlClient;
 using ProyectoArquidiocesis.Datos;
 using System;
 using System.Collections.Generic;
@@ -324,27 +325,14 @@ namespace ProyectoArquidiocesis
         {
 
         }
+        public Microsoft.Office.Interop.Word.Document wordDocument { get; set; }
 
         private void BtnGuardar_Click(object sender, EventArgs e)
         {
-            SautinSoft.PdfMetamorphosis p = new SautinSoft.PdfMetamorphosis();
+            Microsoft.Office.Interop.Word.Application appWord = new Microsoft.Office.Interop.Word.Application();
+            wordDocument = appWord.Documents.Open(@"C:\Archives\newFile.docx");
+            wordDocument.ExportAsFixedFormat(@"C:\Archives\newFile.pdf", WdExportFormat.wdExportFormatPDF);
 
-
-
-            if (p != null)
-            {
-                string docxPath = @"C:\Archives\newFile.docx";
-                string pdfPath = Path.ChangeExtension(docxPath, ".pdf");
-
-
-                // 2. Convert DOCX file to PDF file 
-                if (p.DocxToPdfConvertFile(docxPath, pdfPath) == 0)
-                    System.Diagnostics.Process.Start(pdfPath);
-                else
-                {
-                    MessageBox.Show("Conversion failed!");
-                }
-            }
             if (impresion)
             {
                 try
